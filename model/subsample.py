53ff9d4f6985841004585cd0897462dc47d8c258
Esther <esther.camilo@gntech.med.br>
2024-09-20 15:42:26 -0300

    tentando com 50 de cada ent do modelo base

--- model/subsample.py
+++ model/subsample.py
@@ -2,14 +2,14 @@ s = open('training_bionlp13cg_sub.tsv', 'w')
 
 count = {}
 
-# with open('training_bionlp13cg.tsv') as f:
-with open('fullsamples.tsv') as f:
+with open('training_bionlp13cg.tsv') as f:
+# with open('fullsamples.tsv') as f:
     for l in f:
         line = l.split('\t')
         if line[3] not in count.keys():
             count[line[3]] = 0
-        # if count[line[3]] >= 250:
-        #     continue
+        if count[line[3]] >= 50:
+            continue
         count[line[3]] += 1
         # s.write(l)
 print(count)
